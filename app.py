7e9d0cb9d241491f8f4aa45fe136872c9fed9873
jacoblvf <163808026+jacoblvf@users.noreply.github.com>
2024-07-03 14:06:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,24 @@ df2 = requests.get('https://api.orcascan.com/sheets/L8Wpy42K0h_Mifmb?datetimefor
 df3 = pd.read_csv(io.StringIO(df1.decode('utf-8')))
 df4 = pd.read_csv(io.StringIO(df2.decode('utf-8')))
 
-df3 = df3.groupby(["Name"])[["POM", "Multiplier", "Scan_in"]].agg(POM = ("POM", "max"), Multiplier = ("Multiplier", "max"), Scan_in = ("Scan_in", "sum"))
-df4 = df4.groupby(["Name"])[["Multiplier", "Scan_out"]].agg(Multiplier = ("Multiplier", "max"), Scan_out = ("Scan_out", "sum"))
+df5 = df3.groupby(["Name", "Bulk_or_Indiv"])[["Multiplier", "Scan_in"]].agg(Multiplier = ("Multiplier", "max"), Scan_in = ("Scan_in", "sum"))
+df6 = df4.groupby(["Name", "Bulk_or_Indiv"])[["Multiplier", "Scan_out"]].agg(Multiplier = ("Multiplier", "max"), Scan_out = ("Scan_out", "sum"))
 
-df3 = df3.reset_index()
-df4 = df4.reset_index()
+df5 = df5.reset_index()
+df6 = df6.reset_index()
 
-df5_2 = df3.merge(df4, on=['Name'], suffixes=[None, '_copy'])
-df3 = df3.sort_values(by='Name', ascending=False)
-df4 = df4.sort_values(by='Name', ascending=False)
-df5 = pd.concat([df3,df4["Scan_out"]], axis=1)
-df5=df5_2
+df7_2 = df5.merge(df6, on=['Name', 'Bulk_or_Indiv'], suffixes=[None, '_copy'])
+df5 = df5.sort_values(by='Name', ascending=False)
+df6 = df6.sort_values(by='Name', ascending=False)
+df7 = pd.concat([df3,df4["Scan_out"]], axis=1)
+df7=df7_2
+df7["scan_qty"] = df7["Scan_in"] - df7["Scan_out"]
+df7["indiv_qty"] = df7["scan_qty"]*df7["Multiplier"]
+df8 = df7.groupby(["Name"])[["Bulk_or_Indiv", "indiv_qty"]].agg(bulkindiv = ("Bulk_or_Indiv", lambda x:"Indiv"), qty = ("indiv_qty", "sum"))
+df9 = df8.rename(columns={'bulkindiv': 'Status', 'qty': 'Product Quantity'})
+df9['Product Quantity'] = df9['Product Quantity'].astype("int64")
 
-df5["scan_qty"] = df5["Scan_in"] - df5["Scan_out"]
-df5["indiv_qty"] = df5["scan_qty"]*df5["Multiplier"]
-df6 = df5.groupby(["Name"])[["POM", "indiv_qty"]].agg(POM = ("POM", "max"), qty = ("indiv_qty", "sum"))
-
-df7 = df6.rename(columns={'POM': 'Status', 'qty': 'Product Quantity'})
-df7['Product Quantity'] = df7['Product Quantity'].astype("int64")
-
-st.dataframe(df7, width=1000, height=600)
+st.dataframe(df9, width=1000, height=600)
 
 
 refresh_button = st.button("Refresh")
@@ -37,11 +35,11 @@ if refresh_button:
     st.experimental_rerun()
     
 @st.cache_data
-def convert_df(df7):
+def convert_df(df9):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df7.to_csv().encode('utf-8')
 
-csv = convert_df(df7=df7)
+csv = convert_df(df9=df9)
 
 st.download_button(
     label="Download data as CSV",
